e835a46d8b88ed283c9207cded32761f98359498
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-08-19 15:24:23 +0600

    feat: Header added

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup>
+import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <div class="bg-white dark:bg-slate-800 flex flex-col justify-center items-center w-screen h-screen">
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <h1 class="text-slate-900 dark:text-white text-3xl font-bold underline mt-3">
-    Hello world!
-  </h1>
-  </div>
+  <Header />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="p-4 mb-10 bg-blue-500">
+    <div class="max-w-5xl m-auto">
+      <div class="text-xl font-bold text-white text-center md:text-left">QR Code Generator</div>
+    </div>
+  </header>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
