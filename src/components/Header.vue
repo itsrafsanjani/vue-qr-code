12c963fe4ee2bbaec6c88e6e2aa40287f9df6062
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-08-19 16:27:50 +0600

    feat: easyqrcodejs installed and started using it

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="p-4 mb-10 bg-blue-500">
     <div class="max-w-5xl m-auto">
-      <div class="text-xl font-bold text-white text-center md:text-left">QR Code Generator</div>
+      <a href="/" class="text-xl font-bold text-white text-center md:text-left">QR Code Generator</a>
     </div>
   </header>
 </template>
