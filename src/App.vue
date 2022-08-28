8cc82c5e169d1b1329855432648e1858da0eb29e
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-08-28 22:49:23 +0600

    feat: pwa with offline support done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Header from "./components/Header.vue";
 import Main from "./components/Main.vue";
+import ReloadPrompt from "./components/ReloadPrompt.vue";
 </script>
 
 <template>
   <div class="bg-white dark:bg-slate-900 dark:text-white min-h-screen">
     <Header />
     <Main />
+    <ReloadPrompt />
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,4 +2,7 @@ import { ViteSSG } from 'vite-ssg/single-page'
 import App from './App.vue'
 import './index.css'
 
+if (typeof window !== 'undefined')
+    import('./pwa')
+
 export const createApp = ViteSSG(App)
